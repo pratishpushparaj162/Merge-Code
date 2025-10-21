4e4c01fbb913d41f8ad385c59a0a071d55fe2427
pratishpushparaj162 <pratish.pushparaj16@gmail.com>
2025-10-20 22:42:02 -0400

    Clean up comments in SimpleMerge.java
    Removed unnecessary comments from SimpleMerge.java.

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -1,9 +1,4 @@
-/**
- * Created by Teacher on 1/28/2019.
- * simpleMerge takes in two sorted arrays of
- * integers and merges them to return one sorted
- * array of integers.
- */
+
 public class SimpleMerge {
     public static int[] simpleMerge(int[] arr1, int[] arr2) {
         int arrLength = arr1.length+arr2.length;
